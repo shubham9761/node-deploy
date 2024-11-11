5c9948061275fcf3c8a3b596581242f887def655
Shubham Mishra <shubham09761@gmail.com>
2024-11-11 12:32:41 +0530

    node-9

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const path = require('path');
+
 
 // const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../data.json'), 'utf-8'));
 const users = data.users;
 
 
